c1a4331c44de9272e0d5d84dda001bb78fd91adc
alina26122006 <144228838+alina26122006@users.noreply.github.com>
2024-05-08 08:37:34 +0300

    проект загружен(допы)

---
+++ src/main/java/com/simpleRestApi/demo/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.simpleRestApi.demo.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.simpleRestApi.demo.model.TemplateModel;
+import org.springframework.http.ResponseEntity;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/rest/home")
+public class HomeController {
+    @ResponseBody
+    @RequestMapping(value="", method=RequestMethod.GET)
+    public ResponseEntity<TemplateModel> respones() {
+
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+
+        TemplateModel model = new TemplateModel();
+        model.setMessage("json-ответ");
+        model.setTime((timestamp.toString()));
+
+        return ResponseEntity.ok(model);
+    }
+}
---
+++ src/main/java/com/simpleRestApi/demo/model/TemplateModel.java
@@ -0,0 +1,28 @@
+package com.simpleRestApi.demo.model;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemplateModel {
+    private String message;
+    private String time;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
